bfa86aaeb60050fe0392a422ec36a850b647a939
Rkthak <rkon2609@gmail.com>
2026-06-14 11:36:58 +0530

    response object with response header and status-codes

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const http = require("http");
 
 // create HTTP
 const server = http.createServer((request, response) => {
-  response.write("hello world");
+  response.writeHead(200, { "content-type": "text/html" });
+  response.write(`<!DocType html>
+    <html>
+    <head>
+    <title>NODEJS</title>
+    </head>
+    <body>
+    <h1>Hello World </h1>
+    </body>
+    </html>
+    `);
   response.end();
 });
 
